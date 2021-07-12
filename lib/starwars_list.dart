53253a41136c04d6beca9c7a338259a333b374e8
Uekiz <kingghostdragon@gmail.com>
2021-07-12 19:17:20 +0700

    hello

---
+++ lib/starwars_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarwarsList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _StarwarsListState();
+  }
+}
+
+class _StarwarsListState extends State<StarwarsList> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("hello world");
+  }
+}
