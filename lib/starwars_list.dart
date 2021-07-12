ec9adcd2dbd81af11245d04aaf4e87cb4e55ea12
Uekiz <kingghostdragon@gmail.com>
2021-07-12 20:30:45 +0700

    fetched all but > 82 items get error

--- lib/starwars_list.dart
+++ lib/starwars_list.dart
@@ -14,6 +14,10 @@ class _StarwarsListState extends State<StarwarsList> {
   final StarwarsRepo repo;
   late int page;
   late List<People> people;
+  late bool _error;
+  late bool _loading;
+  late bool _hasMore;
+  final int _nextPageThreshold = 5;
 
   _StarwarsListState() : repo = StarwarsRepo();
 
@@ -23,34 +27,91 @@ class _StarwarsListState extends State<StarwarsList> {
     page = 1;
     people = [];
     fetchPeopleFromRepo();
+    _hasMore = true;
+    _error = false;
+    _loading = true;
   }
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: people.length,
-        itemBuilder: (context, index) {
-          final People person = people[index];
-          return Card(
-            child: Column(
-              children: <Widget>[
-                // Image.network(
-                //   person.thumbnailUrl,
-                //   fit: BoxFit.fitWidth,
-                //   width: double.infinity,
-                //   height: 160,
-                // ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Text(person.name,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                ),
-              ],
-            ),
-          );
-        });
+    if (people.isEmpty) {
+      if (_loading) {
+        return Center(
+            child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: CircularProgressIndicator(),
+        ));
+      } else if (_error) {
+        return Center(
+            child: InkWell(
+          onTap: () {
+            setState(() {
+              _loading = true;
+              _error = false;
+              fetchPeopleFromRepo();
+            });
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Text("Error while loading photos, tap to try agin"),
+          ),
+        ));
+      }
+    } else {
+      return ListView.builder(
+          itemCount: people.length + (_hasMore ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == people.length - _nextPageThreshold) {
+              fetchPeopleFromRepo();
+            }
+            if (index == people.length) {
+              if (_error) {
+                return Center(
+                    child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      _loading = true;
+                      _error = false;
+                      fetchPeopleFromRepo();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text("Error while loading photos, tap to try agin"),
+                  ),
+                ));
+              } else {
+                return Center(
+                    child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: CircularProgressIndicator(),
+                ));
+              }
+            }
+            final People person = people[index];
+            return Card(
+              child: Column(
+                children: <Widget>[
+                  // Image.network(
+                  //   person.thumbnailUrl,
+                  //   fit: BoxFit.fitWidth,
+                  //   width: double.infinity,
+                  //   height: 160,
+                  // ),
+                  Padding(
+                    padding: const EdgeInsets.all(50),
+                    child: Text(person.name,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16)),
+                  ),
+                ],
+              ),
+            );
+          });
+    }
+    return Container();
   }
+  
 
   Future<void> fetchPeopleFromRepo() async {
     print("we're in fetchPeopleFromRepo");
