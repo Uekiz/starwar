6971786488dc4046ce0a08b41cafd36519cadb76
Uekiz <kingghostdragon@gmail.com>
2021-07-12 19:54:30 +0700

    dio

--- lib/starwars_repo.dart
+++ lib/starwars_repo.dart
@@ -1,5 +1,11 @@
-class StarwarsRepo {
+import 'package:dio/dio.dart';
 
+class StarwarsRepo {
+  Future<List<People>> fetchPeople(int page ) async {
+    var response = await Dio().get('https://swapi.dev/api/people/?page=$page');
+    List<People> listPeople = People.toList(response.data['results']);
+    return listPeople;
+  }
 }
 
 class People {
@@ -9,4 +15,12 @@ class People {
   final String gender;
 
   People(this.name, this.height, this.mass, this.gender);
+
+  factory People.fromJson(Map<String, dynamic> json) {
+    return People(json["name"], json["height"], json["mass"], json["gender"]);
+  }
+
+  static List<People> toList(List<dynamic> list) {
+    return list.map((e) => People.fromJson(e)).toList();
+  }
 }
